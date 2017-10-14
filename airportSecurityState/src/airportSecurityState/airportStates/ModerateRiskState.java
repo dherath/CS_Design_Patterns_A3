79a1ac7fbab5849806a9766bf0ab3d70908bbe0e
dinal herath <dinal.bing@gmail.com>
2017-10-14 18:04:39 -0400

    implemented moderate risk state

--- airportSecurityState/src/airportSecurityState/airportStates/ModerateRiskState.java
+++ airportSecurityState/src/airportSecurityState/airportStates/ModerateRiskState.java
@@ -2,9 +2,9 @@ package airportSecurityState.airportStates;
 
 import airportSecurityState.airportSecurity.AirportSecurity;
 
-public class HighRiskState implements AirportStateInterface
+public class ModerateRiskState implements AirportStateInterface
 {
-    private int[] operations;//the operations for high risk state
+    private int[] operations;//the operations for moderate risk state
     private AirportSecurity airportSecurity;
 
 
@@ -12,31 +12,29 @@ public class HighRiskState implements AirportStateInterface
      *Constructor
      *@param the airport security 
      **/
-    public HighRiskState(AirportSecurity airportSecurity){
+    public LowRiskState(AirportSecurity airportSecurity){
 	this.airportSecurity = airportSecurity;
-	this.operations = airportSecurity.getHighRiskOperations();
+	this.operations = airportSecurity.getModerateRiskOperations();
     }
 
     //-------------- state Interface Implementations -------------
     /**
-     *changes the state from high risk to moderate or low if needed
+     *changes the state from moderate risk to low or high risk if needed
      *@param avgTraffic, the average traffic per day
      *@param avgProhibteditems, the average Prohibited items per day
      **/
     public void tightenOrLoosenSecurity(double avgTraffic, double avgProhibtedItems){
-	if( avgTraffic < 8.0 || avgProhibtedItems < 2.0){
-	    if( avgTraffic < 4.0 || avgProhibtedItems < 1.0){
-		airportSecurity.setState(airportSecurity.getLowRiskState());
-	    }
-	    if( avgTraffic >= 4.0 || avgProhibtedItems >= 1.0 ){
-		airportSecurity.setState(airportSecurity.getModerateRiskState());
-	    }
+	if( avgTraffic < 4.0 || avgProhibtedItems < 1.0 ){
+	    airportSecurity.setState(airportSecurity.getLowRiskState());
+	}
+	if( avgTraffic >= 8.0 || avgProhibtedItems >= 2.0){
+	    airportSecurity.setState(airportSecurity.getHighRiskState());
 	}
     }
 
     /**
      *gets the list of operations as a one line string
-     *@return the set of operations for high risk
+     *@return the set of operations for moderate risk
      **/
     public String getResponse(){
 	String temp = "";
