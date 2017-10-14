73243ec59f3a905519d6c0f02ad0e5f5a448ab6d
dinal herath <dinal.bing@gmail.com>
2017-10-14 11:38:57 -0400

    implemented state interface

---
+++ airportSecurityState/src/airportSecurityState/airportStates/AirportStateInterface.java
@@ -0,0 +1,6 @@
+package airportSecurityState.
+
+public interface AirportStateInterface{
+    void tightenOrLoosenSecurity(double avgTraffic, double avgProhibtedItems);//per day
+    String getOperations();//returns the set of operations per threat level
+}
