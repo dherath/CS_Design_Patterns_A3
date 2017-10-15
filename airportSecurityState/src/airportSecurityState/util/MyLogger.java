bc8ce01185e14523d958a84693ba3c23b4fe958d
dinal herath <dinal.bing@gmail.com>
2017-10-15 18:36:51 -0400

    coded logger

--- airportSecurityState/src/airportSecurityState/util/MyLogger.java
+++ airportSecurityState/src/airportSecurityState/util/MyLogger.java
@@ -4,40 +4,41 @@ public class MyLogger{
 
     /*DEBUG_VALUE=4 [Print to stdout everytime a constructor is called]
       DEBUG_VALUE=3 [Print to stdout everytime the state is changed]
-      DEBUG_VALUE=2 [FIXME: add your own scheme here]
-      DEBUG_VALUE=1 [FIXME: add your own scheme here]
-      DEBUG_VALUE=0 [No output should be printed from the applicatio to stdout. It is ok to write to the output file though" ]
+      DEBUG_VALUE=2 [Prints the calculated values for every read  line]
+      DEBUG_VALUE=1 [Prints the read parameters for each line]
+      DEBUG_VALUE=0 [No output is printed to stdout, but output is sent to file ]
     */
 
-    public static enum DebugLevel {RELEASE, FROM_RESULTS, IN_RESULTS, IN_RUN, CONSTRUCTOR
-                                   };
+    public static enum DebugLevel {RELEASE, INPUTS, PARAMETERS, STATE_CHANGE, CONSTRUCTOR
+    };
 
     private static DebugLevel debugLevel;
 
 
     public static void setDebugValue (int levelIn) {
-    switch (levelIn) {
-      case 4: debugLevel = DebugLevel.CONSTRUCTOR; break;
-          // FIXME: add code for other cases
-      case 0: debugLevel = DebugLevel.RELEASE; break;
-    }
+	switch (levelIn){
+	case 4: debugLevel = DebugLevel.CONSTRUCTOR; break;
+	case 3: debugLevel = DebugLevel.STATE_CHANGE; break;
+	case 2: debugLevel = DebugLevel.PARAMETERS; break;
+	case 1: debugLevel = DebugLevel.INPUTS; break;
+	case 0: debugLevel = DebugLevel.RELEASE; break;
+	}
     }
 
     public static void setDebugValue (DebugLevel levelIn) {
-    debugLevel = levelIn;
+	debugLevel = levelIn;
     }
 
     // @return None
-    public static void writeMessage (String  message  ,
-                                     DebugLevel levelIn ) {
-    if (levelIn == debugLevel)
-        System.out.println(message);
+    public static void writeMessage (String message, DebugLevel levelIn ) {
+	if (levelIn == debugLevel)
+	    System.out.println(message);
     }
 
     /**
      * @return String
      */
     public String toString() {
-    return "Debug Level is " + debugLevel;
+	return "Debug Level is " + debugLevel;
     }
 }
