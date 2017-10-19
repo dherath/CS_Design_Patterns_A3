8749abaab838c89263ab97cd8170a2037286ac05
dinal-kuppiya <dinal.bing@gmail.com>
2017-10-19 10:39:37 -0400

    changed calculation locations from context class to StateHelper class

--- airportSecurityState/src/airportSecurityState/airportStates/ModerateRiskState.java
+++ airportSecurityState/src/airportSecurityState/airportStates/ModerateRiskState.java
@@ -1,6 +1,7 @@
 package airportSecurityState.airportStates;
 
 import airportSecurityState.airportSecurity.AirportSecurity;
+import airportSecurityState.airportStates.StateHelper;
 import airportSecurityState.util.MyLogger;
 
 public class ModerateRiskState implements AirportStateInterface
@@ -8,15 +9,17 @@ public class ModerateRiskState implements AirportStateInterface
     private int[] operations;//the operations for moderate risk state
     private AirportSecurity airportSecurity;
     private MyLogger logger;
+    private StateHelper stateHelper;//stat helper
 
     /**
      *Constructor
      *@param the airport security 
      **/
-    public ModerateRiskState(AirportSecurity airportSecurity,MyLogger loggerIn){
+    public ModerateRiskState(AirportSecurity airportSecurity,MyLogger loggerIn,StateHelper stateHelperIn){
 	this.airportSecurity = airportSecurity;
 	this.operations = new int[]{2,3,5,8,9};
 	this.logger = loggerIn;
+	this.stateHelper = stateHelperIn;
 	logger.writeMessage("constructed ModerateRiskState class",logger.converToDebugVal(4));
     }
 
@@ -25,7 +28,7 @@ public class ModerateRiskState implements AirportStateInterface
      *changes the state from moderate risk to low or high risk if needed
      *@param the parameters
      **/
-    public void tightenOrLoosenSecurity(int[] parameters){
+    public void tightenOrLoosenSecurity(String lineIn){
 	int flag = 0;
 	String[] data = stateHelper.preProcessLine(lineIn);
 	stateHelper.updateParameters(data[0],data[1],data[2]);
